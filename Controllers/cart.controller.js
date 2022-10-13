46daa784e9683fca0d73b821d05e1cca3ab8ed8f
LokendraSingh <lokendrasingh0712@gmail.com>
2022-10-14 00:30:27 +0530

    update cost in cart

--- Controllers/cart.controller.js
+++ Controllers/cart.controller.js
@@ -1,4 +1,5 @@
-const { Cart, User } = require("../models")
+const { response } = require("express");
+const { Cart, User, Product } = require("../models")
 
 
 exports.create=async (req,res)=>{
@@ -16,13 +17,108 @@ exports.create=async (req,res)=>{
     res.send({message:"cart created successfully"})  
 }
 
-exports.findCart=async (req,res)=>{
-    
+
+exports.update=async (req,res)=>{
+
     const userId=req.user.id;
+   
+    const user=await User.findByPk(userId);
 
-    const user = await User.findByPk(userId);
+    const cart=await user.getCart();
+    if(!cart){
+        res.send({message:"no cart is associated with this user"})
+    }
 
-    const cart = await user.getCart();
+    const userCart=await Cart.findByPk(cart.id)
+
+    const newproducts=await Product.findAll({where:{id:req.body.products}})
+
+    if(!newproducts){
+       return res.send({message:"no product exists in the given product Id"})
+    }
+
+    const existingProducts=await userCart.getProducts();
+    console.log(existingProducts)
+
+    const updatedProducts=[...existingProducts,...newproducts]
+
+    let totalCost=findTotalCost(updatedProducts)
+    
+    Cart.update({cost:totalCost},{where:{
+        id:cart.id
+}})
+    
+    userCart.setProducts(updatedProducts)
+
+    res.send(updatedProducts)
+
+
+}
+
+
+
+
+exports.findCart=async (req,res)=>{
+    const {cart,products}=await findProductsAndCart(req.user.id)
+
+    res.send({products,totalCost:cart.cost})
+}
+
+exports.deleteProductFromCart=async (req,res)=>{
+
+    const productId=parseInt(req.params.id);
+
+    const userId=req.user.id;
+   
+    const user=await User.findByPk(userId);
+
+    const cart=await user.getCart()
+    if(!cart){
+        res.send({message:"no cart is associated with this user"})
+    }
+
+    const userCart=await Cart.findByPk(cart.id)
+    const existingProducts=await userCart.getProducts()
+
+    const updatedProducts=existingProducts.filter((product)=>{
+      return product.id!==productId;
+       })
      
-    res.send(cart);
+    const totalCost= findTotalCost(updatedProducts)
+   Cart.update({cost:totalCost},{where:{
+        id:cart.id
+    }})  
+    await userCart.setProducts(updatedProducts);
+    res.send(updatedProducts); 
+}
+
+var findProductsAndCart= async (userId)=>{
+   
+    const user=await User.findByPk(userId);
+
+    const cart=await user.getCart()
+    if(!cart){
+        res.send({message:"no cart is associated with this user"})
+    }
+
+    const userCart=await Cart.findByPk(cart.id)
+
+
+    const existingProducts=await userCart.getProducts();
+    //res.send(existingProducts)
+
+    return ({cart:userCart,products:existingProducts})
+
+
+}
+
+
+const findTotalCost=(products)=>{
+    let cost=0;
+
+    for(let i=0;i<products.length;i++){
+        cost+=products[i].cost
+    }
+
+    return cost;
 }
